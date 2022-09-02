6bc1012748efeb732ee72485eedf3501198638df
Jone23 <jone23@rambler.ru>
2022-09-02 06:36:56 +0300

    add loop while

--- src/Loop.java
+++ src/Loop.java
@@ -1,5 +1,11 @@
 public class Loop {
     public static void main(String[] args) {
-        
+        int a = 1;
+
+        while (a<=1000){
+            System.out.println(a);
+            a++;
+        }
+
     }
 }
