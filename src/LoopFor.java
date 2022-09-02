4f16225fa17f96a3dfa56db45e99da0113580c4c
Jone23 <jone23@rambler.ru>
2022-09-02 07:00:03 +0300

    HomeWark7

--- src/LoopFor.java
+++ src/LoopFor.java
@@ -1,7 +1,9 @@
 public class LoopFor {
     public static void main(String[] args) {
-        for (int i = 1000; i>0; i--){
-            if(i % 3 ==0 )System.out.println(i);
+        for (int i = 100; i<=1000; i++){
+            if(i % 2 !=0 && i % 5 ==0){
+                 System.out.println(i);
+            }
         }
     }
 }
